5da1cee19c569f8cc78af3ae090ed517def095f6
Nayeem Abdul Qaiyum <qaiyum.1416@gmail.com>
2024-02-22 23:24:13 +0600

    js update

--- function.js
+++ function.js
@@ -1,27 +1,61 @@
-function getValue(id){
+function getValue(id) {
     const targetElement = document.getElementById(id).innerText;
     return parseInt(targetElement);
 }
 
-const totalSeats =document.getElementsByClassName('seat');
+const totalSeats = document.getElementsByClassName('seat');
 
-for (const seat of totalSeats){
-    seat.addEventListener("click",function(event){
+for (const seat of totalSeats) {
+    seat.addEventListener("click", function (event) {
         const seatName = event.target.innerText;
 
         const seatPrice = 550;
-        // console.log(typeof seatPrice);
 
         const append = document.getElementById("append-child");
 
+
+        // seat count backword
+        const seatLeft = getValue("seat-lefts");
+        document.getElementById("seat-lefts").innerText = seatLeft - 1;
+
+        // seat count forward
+        const selectedSeat = getValue("select-total-seat");
+        document.getElementById("select-total-seat").innerText = selectedSeat + 1;
+
+        // not more than 4 seat selected
+        const atleastSelectedSeat = getValue("select-total-seat");
+        if (atleastSelectedSeat + 1 > 4) {
+            alert("limit cross (you can select 4 seat per-person)");
+            return;
+        }
+
+        // not more than one seat select at a time
+        event.target.setAttribute("disabled", false);
+        event.target.style.backgroundColor = "green";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         const create = document.createElement('create');
-        
+
         // 
         create.classList.add('flex');
         create.classList.add('gap-20');
-        // create.classList.add('relative');
-        // create.classList.add(' justify-evenly');
-        //
+
 
         const text1 = document.createElement('t');
         const text2 = document.createElement('t');
@@ -38,12 +72,37 @@ for (const seat of totalSeats){
         append.appendChild(create);
         totalCost(seatPrice);
 
+        updateTotal();
+
     })
+}
 
-    function totalCost(value) {
-        const totalExpense= getValue("total-price");
+function totalCost(value) {
+    const totalExpense = getValue("total-price");
 
-        const total = totalExpense + parseInt(value);
-        document.getElementById("total-price").innerText = total;
-      }
+    const total = totalExpense + parseInt(value);
+    document.getElementById("total-price").innerText = total;
+}
+
+
+function updateTotal(price) {
+    const totalCost = getValue("total-price");
+    if (price == undefined) {
+        document.getElementById("update-total").innerText = totalCost;
+    }
+    else {
+        const coupon = document.getElementById("coupon").value;
+
+        if (coupon == "NEW15") {
+            const discount = totalCost * 0.15;
+            document.getElementById("update-total").innerText = totalCost - discount;
+        }
+        else if (coupon == "Couple 20") {
+            const discount = totalCost * 0.2;
+            document.getElementById("update-total").innerText = totalCost - discount;
+        }
+        else {
+            alert("Wrong Coupon Code.")
+        }
+    }
 }
